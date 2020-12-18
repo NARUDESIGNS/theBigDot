3b6623100205fecd84def620d6164f86098ceb81
Naru Designs <ibeabuchipaul@gmail.com>
2020-12-18 07:38:38 +0100

    theBigDot v2.0.0

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 //RESOURCES
-const clock = document.getElementById('clock');
+const logo = document.getElementById('logo');
+const themeBtn = document.getElementById('theme-btn');
+const overallContainer = document.getElementById('overall-container');
 const clockFrame = document.getElementById('clock-frame');
+const clock = document.getElementById('clock');
 const hour = document.getElementById('hour');
 const minute = document.getElementById('minute');
-const second = document.getElementById('second');
 const minuteContainer = document.getElementById('minute-container');
+const overallMinuteContainer = document.getElementById('overall-minute-container');
 const secondContainer = document.getElementById('second-container');
+const secondIndicator = document.getElementById('second-indicator');
 const chooseColor = document.getElementById('choose-color');
 const pickColor = document.getElementById('pick-color');
 const dim = document.getElementById('dim');
-const frame = document.getElementById('use-frame');
 const colors = document.getElementsByClassName('colors');
+const dateContainer = document.getElementById('date-container');
+const date = document.getElementById('date');
 
 //document.documentElement.style.setProperty('--touch-color', 'lime');
 
@@ -27,13 +32,13 @@ setInterval( () => {
 
     // rotation calculation
     secondContainer.style.transform = `rotate(${degSecs}deg)`;
-    minuteContainer.style.transform = `rotate(${degMins}deg)`;
-    minute.style.transform = `rotate(${-degMins}deg)`;
+    overallMinuteContainer.style.transform = `rotate(${degMins}deg)`;
+    minuteContainer.style.transform = `rotate(${-degMins}deg)`;
     if(hr == "0") hr = 12; //when time is 00hrs, display 12
     hour.innerText = `${hr > 12 ? hr - 12 : hr}`;
     
-    minute.firstElementChild.innerText = min;
-    minute.firstElementChild.innerText = `${minute.firstElementChild.innerText < 10 ? '0' : ''}${minute.firstElementChild.innerText}`;
+    minuteContainer.firstElementChild.innerText = min;
+    minute.innerText = `${minute.innerText < 10 ? '0' : ''}${minute.innerText}`;
 }, 100);
 
 //choose color button
@@ -49,13 +54,61 @@ chooseColor.addEventListener('click', () => {
 //colors
 for(let color of colors){
     color.addEventListener('click', () => {
-        if(color.id == 'green') document.documentElement.style.setProperty('--color-fill', '#98c5a7');
-        if(color.id == 'orange') document.documentElement.style.setProperty('--color-fill', '#f0b863');
-        if(color.id == 'pink') document.documentElement.style.setProperty('--color-fill', '#ca8b98');
-        if(color.id == 'brown') document.documentElement.style.setProperty('--color-fill', '#a0886c');
-        if(color.id == 'grey') document.documentElement.style.setProperty('--color-fill', '#929292');
-        if(color.id == 'blue') document.documentElement.style.setProperty('--color-fill', '#8364ac');
+        if(color.id == 'green') document.documentElement.style.setProperty('--text-color', '#98c5a7');
+        if(color.id == 'orange') document.documentElement.style.setProperty('--text-color', '#f0b863');
+        if(color.id == 'pink') document.documentElement.style.setProperty('--text-color', '#ca8b98');
+        if(color.id == 'brown') document.documentElement.style.setProperty('--text-color', '#a0886c');
+        if(color.id == 'grey') document.documentElement.style.setProperty('--text-color', '#929292');
+        if(color.id == 'purple') document.documentElement.style.setProperty('--text-color', '#8364ac');
         dim.style.display = "none";
         pickColor.style.display = "none";
     })
 }
+
+//set date
+date.innerText = new Date().toDateString();
+
+//light / dark mode toggle
+let lightMode = true;
+themeBtn.addEventListener('click', ()=> {
+    //toggle light / dark icons
+    themeBtn.firstElementChild.classList.toggle('hidden');    
+    themeBtn.lastElementChild.classList.toggle('hidden'); 
+
+    if(lightMode){
+        themeBtn.style.backgroundColor = 'var(--dark-bg-fill)';
+        pickColor.style.backgroundColor = 'var(--dark-bg-fill)';
+
+        //set dark logo and background color 
+        logo.style.color = 'var(--dark-logo)';
+        overallContainer.style.backgroundColor = 'var(--dark-bg)';
+
+        //set clock elements colors | set date background and choose-color button text colors
+        clockFrame.style.backgroundColor = 'var(--dark-bg)';
+        clock.style.backgroundColor = 'var(--dark-bg)';
+        minute.style.color = 'var(--dark-bg)';
+        secondIndicator.style.borderColor = 'var(--dark-bg)';
+        dateContainer.style.backgroundColor = 'var(--dark-bg-fill)';
+        chooseColor.style.color = 'var(--dark-bg)';
+
+        lightMode = !lightMode;
+    }
+    else {
+        themeBtn.style.backgroundColor = 'var(--light-bg-fill)';
+        pickColor.style.backgroundColor = 'var(--light-bg-fill)';
+
+        //set dark logo and background color 
+        logo.style.color = 'var(--light-logo)';
+        overallContainer.style.backgroundColor = 'var(--light-bg)';
+
+        //set clock elements colors | set date background and choose-color button text colors
+        clockFrame.style.backgroundColor = 'white';
+        clock.style.backgroundColor = 'white';
+        minute.style.color = 'white';
+        secondIndicator.style.borderColor = 'white';
+        dateContainer.style.backgroundColor = 'var(--light-bg-fill)';
+        chooseColor.style.color = 'white';
+
+        lightMode = !lightMode;
+    }
+})
